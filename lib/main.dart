7abf7f397c21d35da65c5dd57d50f14c923d2a05
carlstep <carlstep333@me.com>
2021-05-15 22:59:55 +0200

    added text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,14 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal[300],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 50.0,
+                radius: 60.0,
                 backgroundImage: AssetImage('assets/images/profile_picA.png'),
+              ),
+              SizedBox(
+                height: 10.0,
               ),
               Text(
                 'Cal Stephens',
@@ -29,15 +33,85 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'Flutter & Dart Newbie',
-                style: GoogleFonts.balooThambi(
+                'FLUTTER & DART DEVELOPER',
+                style: GoogleFonts.balooTammudu(
                   //fontFamily: 'Lobster',
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w300,
+                  fontSize: 24.0,
+                  //fontWeight: FontWeight.w100,
                   letterSpacing: .1,
-                  color: Colors.black,
+                  color: Colors.grey[800],
                 ),
-              )
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[100],
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[100],
+                ),
+                //color: Colors.yellowAccent[100],
+                //width: 250.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 80.0),
+                padding: EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone_iphone,
+                      color: Colors.teal,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+324 666 9999 21',
+                      style: GoogleFonts.balooTammudu(
+                        fontSize: 18.0,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.teal[800],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[100],
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[100],
+                ),
+                //color: Colors.yellowAccent[100],
+                //width: 250.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 80.0),
+                padding: EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.email_outlined,
+                      color: Colors.teal,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'cal333@incode.com',
+                      style: GoogleFonts.balooTammudu(
+                        fontSize: 18.0,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.teal[800],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
